5650a3c4d2af11f8857d2073dcb3162922b0afc3
alphonseb <alphonse.bouy@gmail.com>
2018-11-04 02:48:37 +0100

    :sparkles: volume drag control :sparkles: seekbar drag control :sparkles: thumbnails on hover :sparkles: time elapsed implemented

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,19 +17,16 @@ var Player = function () {
             poster: parameters.poster,
             class: parameters.class
         };
+        this.controls = {};
         this.init();
     }
 
     _createClass(Player, [{
         key: 'init',
         value: function init() {
-            var _this = this;
-
             this.buildPlayer();
-            this.$video.addEventListener('click', function () {
-                _this.doPlayPause();
-            });
-            this.buildToolBar();
+            this.buildToolbar();
+            this.initControls();
         }
     }, {
         key: 'buildPlayer',
@@ -60,10 +57,212 @@ var Player = function () {
                 }
             }
 
-            var $container = document.createElement('div');
-            $container.classList.add('player');
-            $container.appendChild(this.$video);
-            document.body.appendChild($container);
+            this.$player = document.createElement('div');
+            this.$player.classList.add('player');
+            this.$player.appendChild(this.$video);
+            document.body.appendChild(this.$player);
+        }
+    }, {
+        key: 'buildToolbar',
+        value: function buildToolbar() {
+            /**
+             * Seekbar
+             */
+            this.$seekbar = document.createElement('div');
+            this.$seekbar.classList.add('player__seekbar');
+            this.$player.appendChild(this.$seekbar);
+
+            this.$seekbarFiller = document.createElement('div');
+            this.$seekbarFiller.classList.add('filler');
+            this.$seekbar.appendChild(this.$seekbarFiller);
+
+            /**
+             * Seekbar Thumbnail
+             */
+
+            this.$seekbarThumbnail = document.createElement('video');
+            this.$seekbarThumbnail.classList.add('player__seekbar__thumbnail');
+            this.$seekbar.appendChild(this.$seekbarThumbnail);
+            this.$seekbarThumbnail.src = this.state.src;
+
+            /**
+             * Controls container
+             */
+            this.controls.$toolbar = document.createElement('div');
+            this.controls.$toolbar.classList.add('player__toolbar');
+            this.$player.appendChild(this.controls.$toolbar);
+
+            /**
+             * Time display
+             */
+            this.controls.$timeDisplay = document.createElement('p');
+            this.controls.$timeDisplay.classList.add('player__toolbar__time-display');
+            this.controls.$toolbar.appendChild(this.controls.$timeDisplay);
+
+            /**
+             * Play button
+             */
+            this.controls.$playpauseButton = document.createElement('button');
+            this.controls.$playpauseButton.textContent = 'Play';
+            this.controls.$playpauseButton.classList.add('player__toolbar__playpause-button');
+            this.controls.$toolbar.appendChild(this.controls.$playpauseButton);
+
+            /**
+             * Volume
+             */
+
+            this.controls.volume = {};
+
+            this.controls.volume.$container = document.createElement('div');
+            this.controls.volume.$container.classList.add('player__toolbar__volume-container');
+            this.controls.$toolbar.appendChild(this.controls.volume.$container);
+
+            this.controls.volume.$muteButton = document.createElement('button');
+            this.controls.volume.$muteButton.textContent = 'Mute';
+            this.controls.volume.$muteButton.classList.add('player__toolbar__mute-button');
+            this.controls.volume.$container.appendChild(this.controls.volume.$muteButton);
+
+            this.controls.volume.$slider = document.createElement('div');
+            this.controls.volume.$slider.classList.add('player__toolbar__volume-container__volume-slider');
+            this.controls.volume.$sliderFiller = document.createElement('div');
+            this.controls.volume.$sliderFiller.classList.add('filler');
+            this.controls.volume.$slider.appendChild(this.controls.volume.$sliderFiller);
+            this.controls.volume.$container.appendChild(this.controls.volume.$slider);
+
+            /**
+             * Fullscreen
+             */
+
+            this.controls.$fullscreenButton = document.createElement('button');
+            this.controls.$fullscreenButton.classList.add('player__toolbar__fullscreen-button');
+            this.controls.$fullscreenButton.textContent = 'Full Screen';
+            this.controls.$toolbar.appendChild(this.controls.$fullscreenButton);
+        }
+    }, {
+        key: 'initControls',
+        value: function initControls() {
+            var _this = this;
+
+            /**
+             * Seekbar running
+             */
+
+            var seekbarRun = function seekbarRun() {
+                var playedRatio = _this.$video.currentTime / _this.$video.duration;
+                _this.$seekbarFiller.style.transform = 'scaleX(' + playedRatio + ')';
+                window.requestAnimationFrame(seekbarRun);
+            };
+            seekbarRun();
+
+            /**
+             * Seekbar interaction
+             */
+            var seekbarRect = this.$seekbar.getBoundingClientRect();
+
+            //Time movement
+            var isSeekbarClicked = false;
+            this.$seekbar.addEventListener('mousedown', function (e) {
+                isSeekbarClicked = true;
+                var clickedTime = Math.floor((e.clientX - seekbarRect.left) / seekbarRect.width * _this.$video.duration);
+                _this.$video.currentTime = clickedTime;
+            });
+            window.addEventListener('mousemove', function (e) {
+                if (isSeekbarClicked) {
+                    var time = Math.floor((e.clientX - seekbarRect.left) / seekbarRect.width * _this.$video.duration);
+                    time > _this.$video.duration ? time = _this.$video.duration : '';
+                    time < 0 ? time = 0 : '';
+                    _this.$video.currentTime = time;
+                }
+            });
+            window.addEventListener('mouseup', function () {
+                isSeekbarClicked = false;
+            });
+
+            //Thumbnail
+            this.$seekbar.addEventListener('mousemove', function (e) {
+                var xPosition = e.clientX - seekbarRect.left;
+                var time = Math.floor(xPosition / seekbarRect.width * _this.$video.duration);
+
+                _this.$seekbarThumbnail.currentTime = time;
+                _this.$seekbarThumbnail.style.transform = 'translateX(' + xPosition + 'px)';
+            });
+
+            /**
+             * Time Display
+             */
+            var displayCurrentTime = function displayCurrentTime() {
+                var timeElapsed = new Date(null);
+                timeElapsed.setSeconds(Math.floor(_this.$video.currentTime));
+                var totalTime = new Date(null);
+                var duration = _this.$video.duration ? _this.$video.duration : 0;
+
+                totalTime.setSeconds(Math.floor(duration));
+
+                _this.controls.$timeDisplay.textContent = timeElapsed.toISOString().substr(14, 5) + ' / ' + totalTime.toISOString().substr(14, 5);
+                window.requestAnimationFrame(displayCurrentTime);
+            };
+            displayCurrentTime();
+
+            /**
+             * 
+             * Play and pause on video click
+             */
+
+            this.$video.addEventListener('click', function () {
+                _this.doPlayPause();
+            });
+
+            /**
+             * 
+             * Play and pause on button click
+             */
+
+            this.controls.$playpauseButton.addEventListener('click', function () {
+                _this.doPlayPause();
+            });
+
+            /**
+             * Handle Volume
+             */
+
+            //Muting
+            this.controls.volume.$muteButton.addEventListener('click', function () {
+                _this.mute();
+            });
+
+            //Volume slider
+            var volumeSliderRect = this.controls.volume.$slider.getBoundingClientRect();
+
+            var isSliderClicked = false;
+            this.controls.volume.$slider.addEventListener('mousedown', function (e) {
+                isSliderClicked = true;
+                var ratio = (e.clientX - volumeSliderRect.left) / volumeSliderRect.width;
+                _this.controls.volume.$sliderFiller.style.transform = 'scaleX(' + ratio + ')';
+                _this.$video.volume = ratio;
+            });
+            window.addEventListener('mouseup', function () {
+                _this.controls.volume.$sliderFiller.style.transition = 'transform .3s';
+                isSliderClicked = false;
+            });
+            window.addEventListener('mousemove', function (e) {
+                if (isSliderClicked) {
+                    _this.controls.volume.$sliderFiller.style.transition = 'none';
+                    var ratio = (e.clientX - volumeSliderRect.left) / volumeSliderRect.width;
+                    ratio > 1 ? ratio = 1 : '';
+                    ratio < 0 ? ratio = 0 : '';
+                    _this.controls.volume.$sliderFiller.style.transform = 'scaleX(' + ratio + ')';
+                    _this.$video.volume = ratio;
+                }
+            });
+
+            /**
+             * Handle Fullscreen
+             */
+
+            var isFullScreen = false;
+            this.controls.$fullscreenButton.addEventListener('click', function () {
+                isFullScreen = _this.goFullscreen(isFullScreen);
+            });
         }
     }, {
         key: 'doPlayPause',
@@ -71,85 +270,21 @@ var Player = function () {
             this.$video.paused ? this.$video.play() : this.$video.pause();
         }
     }, {
-        key: 'buildToolBar',
-        value: function buildToolBar() {
-            var _this2 = this;
-
-            this.$toolBar = document.createElement('div');
-            this.$toolBar.classList.add('player__toolbar');
-            this.$video.parentNode.appendChild(this.$toolBar);
-
-            var $playpauseButton = document.createElement('button');
-            $playpauseButton.textContent = 'Play';
-            $playpauseButton.classList.add('player__toolbar__playpause-button');
-            this.$toolBar.appendChild($playpauseButton);
-            $playpauseButton.addEventListener('click', function () {
-                _this2.doPlayPause();
-            });
-
-            var $volumeContainer = document.createElement('div');
-            $volumeContainer.classList.add('player__toolbar__volume-container');
-            this.$toolBar.appendChild($volumeContainer);
-
-            var $muteButton = document.createElement('button');
-            $muteButton.textContent = 'Mute';
-            $muteButton.classList.add('player__toolbar__mute-button');
-            $volumeContainer.appendChild($muteButton);
-            $muteButton.addEventListener('click', function () {
-                if (_this2.$video.muted) {
-                    _this2.$video.muted = false;
-                    $volumeSliderFiller.style.transform = 'scaleX(' + _this2.$video.volume + ')';
-                } else {
-                    _this2.$video.muted = true;
-                    $volumeSliderFiller.style.transform = 'scaleX(0)';
-                }
-            });
-
-            var $volumeSlider = document.createElement('div');
-            $volumeSlider.classList.add('player__toolbar__volume-container__volume-slider');
-            var $volumeSliderFiller = document.createElement('div');
-            $volumeSliderFiller.classList.add('filler');
-            $volumeSlider.appendChild($volumeSliderFiller);
-            $volumeContainer.appendChild($volumeSlider);
-
-            var volumeSliderRect = $volumeSlider.getBoundingClientRect();
-
-            var isSliderClicked = false;
-            $volumeSlider.addEventListener('mousedown', function (e) {
-                isSliderClicked = true;
-                var ratio = (e.clientX - volumeSliderRect.left) / volumeSliderRect.width;
-                $volumeSliderFiller.style.transform = 'scaleX(' + ratio + ')';
-                _this2.$video.volume = ratio;
-            });
-            window.addEventListener('mouseup', function () {
-                $volumeSliderFiller.style.transition = 'transform .3s';
-                isSliderClicked = false;
-            });
-            window.addEventListener('mousemove', function (e) {
-                if (isSliderClicked) {
-                    $volumeSliderFiller.style.transition = 'none';
-                    var ratio = (e.clientX - volumeSliderRect.left) / volumeSliderRect.width;
-                    ratio > 1 ? ratio = 1 : '';
-                    ratio < 0 ? ratio = 0 : '';
-                    $volumeSliderFiller.style.transform = 'scaleX(' + ratio + ')';
-                    _this2.$video.volume = ratio;
-                }
-            });
-
-            var $fullscreenButton = document.createElement('button');
-            $fullscreenButton.classList.add('player__toolbar__fullscreen-button');
-            $fullscreenButton.textContent = 'Full Screen';
-            this.$toolBar.appendChild($fullscreenButton);
-            var isFullScreen = false;
-            $fullscreenButton.addEventListener('click', function () {
-                if (isFullScreen) {
-                    document.webkitExitFullscreen();
-                    isFullScreen = false;
-                } else {
-                    isFullScreen = true;
-                    _this2.$video.parentNode.webkitRequestFullScreen();
-                }
-            });
+        key: 'mute',
+        value: function mute() {
+            if (this.$video.muted) {
+                this.$video.muted = false;
+                this.controls.volume.$sliderFiller.style.transform = 'scaleX(' + this.$video.volume + ')';
+            } else {
+                this.$video.muted = true;
+                this.controls.volume.$sliderFiller.style.transform = 'scaleX(0)';
+            }
+        }
+    }, {
+        key: 'goFullscreen',
+        value: function goFullscreen(isFullScreen) {
+            isFullScreen ? document.webkitExitFullscreen() : this.$player.webkitRequestFullScreen();
+            return !isFullScreen;
         }
     }]);
 
--- src/js/Classes/Player.js
+++ src/js/Classes/Player.js
@@ -10,15 +10,14 @@ class Player{
             poster: parameters.poster,
             class: parameters.class
         }
+        this.controls = {}
         this.init()   
     }
 
     init(){
         this.buildPlayer()
-        this.$video.addEventListener('click', ()=>{
-            this.doPlayPause()
-        })
-        this.buildToolBar()
+        this.buildToolbar()
+        this.initControls()
     }
     
     buildPlayer(){
@@ -26,95 +25,232 @@ class Player{
         for (const attribute of Object.keys(this.state)) {
             this.state[attribute] !== undefined ? this.$video.setAttribute(attribute, this.state[attribute]) : ''
         }
-        let $container = document.createElement('div')
-        $container.classList.add('player')
-        $container.appendChild(this.$video)
-        document.body.appendChild($container)       
+        this.$player = document.createElement('div')
+        this.$player.classList.add('player')
+        this.$player.appendChild(this.$video)
+        document.body.appendChild(this.$player)       
     }
+
+    buildToolbar(){
+        /**
+         * Seekbar
+         */
+        this.$seekbar = document.createElement('div')
+        this.$seekbar.classList.add('player__seekbar')
+        this.$player.appendChild(this.$seekbar)
+        
+        this.$seekbarFiller = document.createElement('div')
+        this.$seekbarFiller.classList.add('filler')
+        this.$seekbar.appendChild(this.$seekbarFiller)
+
+        /**
+         * Seekbar Thumbnail
+         */
+
+        this.$seekbarThumbnail = document.createElement('video')
+        this.$seekbarThumbnail.classList.add('player__seekbar__thumbnail')
+        this.$seekbar.appendChild(this.$seekbarThumbnail)
+        this.$seekbarThumbnail.src = this.state.src
+
+        /**
+         * Controls container
+         */
+        this.controls.$toolbar = document.createElement('div')
+        this.controls.$toolbar.classList.add('player__toolbar')
+        this.$player.appendChild(this.controls.$toolbar)
+
+        /**
+         * Time display
+         */
+        this.controls.$timeDisplay = document.createElement('p')
+        this.controls.$timeDisplay.classList.add('player__toolbar__time-display')
+        this.controls.$toolbar.appendChild(this.controls.$timeDisplay)
+        
+        /**
+         * Play button
+         */
+        this.controls.$playpauseButton = document.createElement('button')
+        this.controls.$playpauseButton.textContent = 'Play'
+        this.controls.$playpauseButton.classList.add('player__toolbar__playpause-button')
+        this.controls.$toolbar.appendChild(this.controls.$playpauseButton)
+        
+        /**
+         * Volume
+         */
+
+        this.controls.volume = {}
+
+        this.controls.volume.$container = document.createElement('div')
+        this.controls.volume.$container.classList.add('player__toolbar__volume-container')
+        this.controls.$toolbar.appendChild(this.controls.volume.$container)
+
+        this.controls.volume.$muteButton = document.createElement('button')
+        this.controls.volume.$muteButton.textContent = 'Mute'
+        this.controls.volume.$muteButton.classList.add('player__toolbar__mute-button')
+        this.controls.volume.$container.appendChild(this.controls.volume.$muteButton)
     
-    doPlayPause(){
-        this.$video.paused ? this.$video.play() : this.$video.pause()
+        this.controls.volume.$slider = document.createElement('div')
+        this.controls.volume.$slider.classList.add('player__toolbar__volume-container__volume-slider')
+        this.controls.volume.$sliderFiller = document.createElement('div')
+        this.controls.volume.$sliderFiller.classList.add('filler')
+        this.controls.volume.$slider.appendChild(this.controls.volume.$sliderFiller)
+        this.controls.volume.$container.appendChild(this.controls.volume.$slider)
+
+        /**
+         * Fullscreen
+         */
+
+        this.controls.$fullscreenButton = document.createElement('button')
+        this.controls.$fullscreenButton.classList.add('player__toolbar__fullscreen-button')
+        this.controls.$fullscreenButton.textContent = 'Full Screen'
+        this.controls.$toolbar.appendChild(this.controls.$fullscreenButton)
+        
+
     }
 
-    buildToolBar(){
-        this.$toolBar = document.createElement('div')
-        this.$toolBar.classList.add('player__toolbar')
-        this.$video.parentNode.appendChild(this.$toolBar)
+    initControls(){
 
-        let $playpauseButton = document.createElement('button')
-        $playpauseButton.textContent = 'Play'
-        $playpauseButton.classList.add('player__toolbar__playpause-button')
-        this.$toolBar.appendChild($playpauseButton)
-        $playpauseButton.addEventListener('click',()=>{
+        /**
+         * Seekbar running
+         */
+
+        const seekbarRun = ()=>{
+            let playedRatio = this.$video.currentTime / this.$video.duration
+            this.$seekbarFiller.style.transform = `scaleX(${playedRatio})`
+            window.requestAnimationFrame(seekbarRun)
+        }
+        seekbarRun()
+
+        /**
+         * Seekbar interaction
+         */
+        const seekbarRect = this.$seekbar.getBoundingClientRect()
+
+        //Time movement
+        let isSeekbarClicked = false
+        this.$seekbar.addEventListener('mousedown',(e)=>{
+            isSeekbarClicked = true
+            let clickedTime = Math.floor(((e.clientX - seekbarRect.left)/seekbarRect.width)*this.$video.duration)
+            this.$video.currentTime = clickedTime
+        })
+        window.addEventListener('mousemove',(e)=>{
+            if (isSeekbarClicked) {
+                let time = Math.floor(((e.clientX - seekbarRect.left) / seekbarRect.width) * this.$video.duration)
+                time > this.$video.duration ? time = this.$video.duration : ''
+                time < 0 ? time = 0 : ''
+                this.$video.currentTime = time
+            }
+        })
+        window.addEventListener('mouseup',()=>{
+            isSeekbarClicked = false
+        })
+
+        //Thumbnail
+        this.$seekbar.addEventListener('mousemove',(e)=>{
+            let xPosition = e.clientX - seekbarRect.left;
+            let time = Math.floor(xPosition/seekbarRect.width * this.$video.duration)
+            
+            this.$seekbarThumbnail.currentTime = time
+            this.$seekbarThumbnail.style.transform = `translateX(${xPosition}px)`
+        })
+
+        /**
+         * Time Display
+         */
+        const displayCurrentTime = ()=>{
+            let timeElapsed = new Date(null)
+            timeElapsed.setSeconds(Math.floor(this.$video.currentTime))
+            let totalTime = new Date(null)
+            let duration = this.$video.duration ? this.$video.duration : 0
+            
+            totalTime.setSeconds(Math.floor(duration))
+            
+
+            this.controls.$timeDisplay.textContent = `${timeElapsed.toISOString().substr(14,5)} / ${totalTime.toISOString().substr(14,5)}`
+            window.requestAnimationFrame(displayCurrentTime)
+        }
+        displayCurrentTime()
+
+        /**
+         * 
+         * Play and pause on video click
+         */
+        
+        this.$video.addEventListener('click', () => {
             this.doPlayPause()
         })
 
-        let $volumeContainer = document.createElement('div')
-        $volumeContainer.classList.add('player__toolbar__volume-container')
-        this.$toolBar.appendChild($volumeContainer)
+        /**
+         * 
+         * Play and pause on button click
+         */
 
-
-        let $muteButton = document.createElement('button')
-        $muteButton.textContent = 'Mute'
-        $muteButton.classList.add('player__toolbar__mute-button')
-        $volumeContainer.appendChild($muteButton)
-        $muteButton.addEventListener('click',()=>{
-            if (this.$video.muted) {
-                this.$video.muted = false
-                $volumeSliderFiller.style.transform = `scaleX(${this.$video.volume})`
-            }
-            else{
-                this.$video.muted = true
-                $volumeSliderFiller.style.transform = 'scaleX(0)'
-            }
+        this.controls.$playpauseButton.addEventListener('click', () => {
+            this.doPlayPause()
         })
 
-        let $volumeSlider = document.createElement('div')
-        $volumeSlider.classList.add('player__toolbar__volume-container__volume-slider')
-        let $volumeSliderFiller = document.createElement('div')
-        $volumeSliderFiller.classList.add('filler')
-        $volumeSlider.appendChild($volumeSliderFiller)
-        $volumeContainer.appendChild($volumeSlider)
+        /**
+         * Handle Volume
+         */
 
-        const volumeSliderRect = $volumeSlider.getBoundingClientRect()
+         //Muting
+        this.controls.volume.$muteButton.addEventListener('click', () => {
+            this.mute()
+        })
+
+        //Volume slider
+        const volumeSliderRect = this.controls.volume.$slider.getBoundingClientRect()
 
         let isSliderClicked = false
-        $volumeSlider.addEventListener('mousedown',(e)=>{
+        this.controls.volume.$slider.addEventListener('mousedown', (e) => {
             isSliderClicked = true
-            let ratio = (e.clientX - volumeSliderRect.left)/volumeSliderRect.width
-            $volumeSliderFiller.style.transform = `scaleX(${ratio})`
+            let ratio = (e.clientX - volumeSliderRect.left) / volumeSliderRect.width
+            this.controls.volume.$sliderFiller.style.transform = `scaleX(${ratio})`
             this.$video.volume = ratio
         })
-        window.addEventListener('mouseup',()=>{
-            $volumeSliderFiller.style.transition = 'transform .3s'
+        window.addEventListener('mouseup', () => {
+            this.controls.volume.$sliderFiller.style.transition = 'transform .3s'
             isSliderClicked = false
-            
+
         })
-        window.addEventListener('mousemove',(e)=>{
+        window.addEventListener('mousemove', (e) => {
             if (isSliderClicked) {
-                $volumeSliderFiller.style.transition = 'none'
-                let ratio = (e.clientX - volumeSliderRect.left)/volumeSliderRect.width
+                this.controls.volume.$sliderFiller.style.transition = 'none'
+                let ratio = (e.clientX - volumeSliderRect.left) / volumeSliderRect.width
                 ratio > 1 ? ratio = 1 : ''
                 ratio < 0 ? ratio = 0 : ''
-                $volumeSliderFiller.style.transform = `scaleX(${ratio})`
+                this.controls.volume.$sliderFiller.style.transform = `scaleX(${ratio})`
                 this.$video.volume = ratio
             }
         })
 
-        let $fullscreenButton = document.createElement('button')
-        $fullscreenButton.classList.add('player__toolbar__fullscreen-button')
-        $fullscreenButton.textContent = 'Full Screen'
-        this.$toolBar.appendChild($fullscreenButton)
-        let isFullScreen = false
-        $fullscreenButton.addEventListener('click', ()=>{
-            if (isFullScreen) {
-                document.webkitExitFullscreen()
-                isFullScreen = false
-            } else {
-                isFullScreen = true
-                this.$video.parentNode.webkitRequestFullScreen()
-            }
-        })
+        /**
+         * Handle Fullscreen
+         */
 
+        let isFullScreen = false
+        this.controls.$fullscreenButton.addEventListener('click', () => {
+            isFullScreen = this.goFullscreen(isFullScreen)
+        })
+    }
+
+    doPlayPause(){
+        this.$video.paused ? this.$video.play() : this.$video.pause()
+    }
+
+    mute(){
+        if (this.$video.muted) {
+            this.$video.muted = false
+            this.controls.volume.$sliderFiller.style.transform = `scaleX(${this.$video.volume})`
+        }
+        else {
+            this.$video.muted = true
+            this.controls.volume.$sliderFiller.style.transform = 'scaleX(0)'
+        }
+    }
+
+    goFullscreen(isFullScreen){
+        isFullScreen ? document.webkitExitFullscreen() : this.$player.webkitRequestFullScreen()
+        return !isFullScreen
     }
 }
